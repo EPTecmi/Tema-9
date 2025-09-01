091a1c00e85e029e92bd7b3f73852cfda1de05d4
Edgar Patricio <edgaripatricio@tecmilenio.mx>
2025-09-01 14:28:32 -0600

    Clase ejemplos

---
+++ class.py
@@ -0,0 +1,7 @@
+lista_compras = ["manzanas", "peras", "leche", "huevos"]
+lista_compras.append("uvas")
+lista_compras.insert(0,"pan")
+lista_compras.remove("peras")
+lista_compras.pop()
+lista_compras[2] = "carne"
+print(lista_compras)
